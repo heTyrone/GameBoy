3867b490c1912e3dd293dea03826e4cf2c8edbbd
heTyrone <yuzhe.he@thoughtworks.com>
2021-12-16 22:49:03 +0800

    case for no valid input

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 'use strict';
 
-const clickEventhandler = () => console.log(document.querySelector('.guess').value);
+//true
+// console.log(typeof '22' === 'string');
 
-document.querySelector('.check').addEventListener('click', clickEventhandler);
+//false
+// console.log(typeof '22' === 'String');
 
+// console.log(typeof '22' === String);
+
+// console.log(typeof '22' === string);
+
+const clickEventhandler = () => {
+    const guess = document.querySelector('.guess').value;
+    const guessNumber = parseStringToNumber(guess);
+    console.log(guessNumber);
+    if (!guessNumber) {
+        noValidInput();
+    };
+};
+
+const clickCheckButton = document.querySelector('.check').addEventListener('click', clickEventhandler);
+
+// const parseStringToNumber = (input) => input !== '' && typeof input === 'string' ? Number(input) : 'Input is blank';
+const parseStringToNumber = (input) => input !== '' && typeof input === 'string' ? Number(input) : 0;
+
+const noValidInput = () => document.querySelector('.message').textContent = '🤷‍♂️🤷‍♂️🤷‍♂️🤷‍♂️ No number!';
 
